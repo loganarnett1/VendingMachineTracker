74949991e55055bca774d8f571068ffe6fa7d069
arnettln <59770778+loganarnett1@users.noreply.github.com>
2023-04-13 11:33:34 -0400

    Initial commit

---
+++ VendingMachineTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendingMachineTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
